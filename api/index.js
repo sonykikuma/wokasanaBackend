f8c52f43d0aaa3742280835932ac3b378800b804
Keizh <krishnamulay05@gmail.com>
2024-12-03 17:34:37 +0530

    chore: fixed statusCode on index.js/36

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use("/tags", TagRouter);
 app.use("/report", ReportRouter);
 
 app.get("/", (req, res) => {
-  req.statusCode(200).json({ message: "backend is live" });
+  req.status(200).json({ message: "backend is live" });
 });
 
 app.listen(process.env.PORT || 5500, () => console.log(`Web Server is online`));
