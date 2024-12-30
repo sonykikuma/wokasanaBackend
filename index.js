eda060839c30f890ab4f8c2d1467897aa5bac599
sonykikuma <er.abhasoni@gmail.com>
2024-12-30 11:47:20 +0530

    cors modified

--- index.js
+++ index.js
@@ -14,15 +14,28 @@ initializeDatabase();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://workasana-frontend-red.vercel.app",
+];
+
 app.use(
-  cors()
-  // cors({
-  //   origin: "*",
-  //   methods: ["POST", "GET", "PATCH", "PUT"],
-  //   allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-  //   credentials: true,
-  //   optionsSuccessStatus: 200,
-  // })
+  //cors()
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (e.g., mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["POST", "GET", "PATCH", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
 );
 app.use(express.json());
 app.use(cookieParser());
